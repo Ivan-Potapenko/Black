96af9b7976c79b5c10d4a12d7d9398e3276329dd
1one <71661207+Potapenko-Ivan@users.noreply.github.com>
2021-07-04 04:24:44 +0200

    1
    Initial

---
+++ Assets/Scr/ContScrJ.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ContScrJ : MonoBehaviour {
+  
+    public Collider2D Col;
+    public ConGGScr ContS;
+    public Vector3 ClT;
+    public Vector3 ClT2;
+    public int k = -1;
+    public float z = 1;
+
+
+    void Update()
+    {
+        
+        if ((Input.touches.Length > 0))
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+
+
+                if (((Input.touches[i].phase == TouchPhase.Began)))
+                {
+                    ClT = new Vector3(Camera.main.ScreenToWorldPoint(Input.touches[i].position).x, Camera.main.ScreenToWorldPoint(Input.touches[i].position).y, z);
+                    if (Col.bounds.Contains(ClT))//Col.bounds.Contains(ClT))
+                    {
+                        ContS.Jump();
+                        k = i;
+                    }
+
+                    ClT2 = new Vector3(Camera.main.ScreenToWorldPoint(Input.touches[i].position).x, Camera.main.ScreenToWorldPoint(Input.touches[i].position).y, z);
+                }
+
+            }
+        }
+        
+}
+}
